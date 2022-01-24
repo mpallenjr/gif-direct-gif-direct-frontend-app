e1e9ee5554127cbee725f5b187d3c7aaba3c320f
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-23 18:29:03 -0600

    connect backend to front for index with trending images.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Search for GIF</h1>
+   <p> <input type="text">
+    <button> Search </button> </p> 
+
+<h1> Translate to GIF </h1>
+   <p> <input type="text">
+    <button> Translate </button></p>
+<h1> Trending GIFS </h1>
+  <div v-for="data in trendingGIFS">
+    <div v-for="gif in data">
+      <iframe v-bind:src="gif.embed_url" width="20%" height="20%" style="border:1px solid black;">
+      </iframe>
+      <br/>
+      <br/>
+      </div>
+  </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script>
+import axios from 'axios';
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        trendingGIFS: []
+      };
+    },
+    created: function () {
+      axios.get("http://localhost:3000/trending_gifs").then(response => {
+          console.log(response.data)
+          this.trendingGIFS = response.data
+      }
+      )
+    },
+    methods: {},
+  };
+</script>}
